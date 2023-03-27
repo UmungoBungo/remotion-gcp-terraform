18a5823385d41baf0a42d71a99b2d8b43389fe29
Matt McGillivray <m.mcgillivray@outlook.com>
2023-03-27 13:49:08 +0800

    using locals instead of vars

--- main.tf
+++ main.tf
@@ -13,12 +13,12 @@ data "google_project_metadata" "project" {
   metadata = "project-id"
 }
 
-variable "project_id" {
-  default = "${data.google_project_metadata.project.value}"
+locals {
+  project_id = data.google_project_metadata.project.value
 }
 
 provider "google" {
-  project = var.project_id
+  project = local.project_id
   region  = "us-central1"
   zone    = "us-central1-c"
 }
@@ -52,13 +52,13 @@ resource "google_service_account" "remotion_sa" {
 
 # Bind the IAM role to the service account
 resource "google_project_iam_member" "remotion_sa" {
-  project = var.project_id
+  project = local.project_id
   role    = google_project_iam_custom_role.remotion_sa.id
   member  = "serviceAccount:${google_service_account.remotion_sa.email}"
 }
 
 # Enable Cloud Run API
 resource "google_project_service" "cloud_run" {
-  project = var.project_id
+  project = local.project_id
   service = "run.googleapis.com"
 }
