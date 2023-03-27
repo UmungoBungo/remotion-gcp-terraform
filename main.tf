d86e3feaf285fe8ea9c9d7da961b2c9e61d3defd
Matt McGillivray <m.mcgillivray@outlook.com>
2023-03-27 13:15:37 +0800

    creating role + SA

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+# Enable required APIs
 module "project-services" {
   source  = "terraform-google-modules/project-factory/google//modules/project_services"
   version = "~> 14.2"
@@ -27,4 +28,37 @@ module "project-services" {
 
   disable_services_on_destroy = false
   disable_dependent_services  = false
+}
+
+# Create an IAM role
+resource "google_project_iam_custom_role" "tf_sa_role" {
+  role_id     = "tfRemotionSA"
+  title       = "tf Remotion API Service Account"
+  description = "Allow the service account to manage necessary resources for Remotion Cloud Run rendering."
+  permissions = [
+    "iam.serviceAccounts.actAs",
+    "run.routes.invoke",
+    "run.services.create",
+    "run.services.list",
+    "run.services.update",
+    "storage.buckets.get",
+    "storage.buckets.list",
+    "storage.objects.create",
+    "storage.objects.delete",
+    "storage.objects.list",
+    "run.services.getIamPolicy",
+    "run.services.setIamPolicy"
+  ]
+}
+
+# Create a service account
+resource "google_service_account" "tf_remotion_sa" {
+  account_id   = "tf-remotion-sa"
+  display_name = "Remotion Service Account"
+}
+
+# Bind the IAM role to the service account
+resource "google_project_iam_member" "tf_remotion_sa" {
+  role    = google_project_iam_custom_role.tf_sa_role.id
+  member  = "serviceAccount:${google_service_account.tf_remotion_sa.email}"
 }
